d5a1f13135f172acd646ca464d2784974560d2c6
Daniel <daniel@thebox3d.com>
2025-12-14 11:18:46 -0700

    Add missing Pickleball.xcodeproj and source files for iOS app

---
+++ PickleballTests/PickleballTests.swift
@@ -0,0 +1,17 @@
+//
+//  PickleballTests.swift
+//  PickleballTests
+//
+//  Created by Daniel Shemon on 12/12/25.
+//
+
+import Testing
+@testable import Pickleball
+
+struct PickleballTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
